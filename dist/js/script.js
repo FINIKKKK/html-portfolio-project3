e8f57b99a746472ab8d987674f23cca6c6cdd47f
DIGIKRASH <krashmate@gmail.com>
2022-04-07 22:43:42 +0600

    порядок, cursor, language

--- dist/js/script.js
+++ dist/js/script.js
@@ -48,6 +48,7 @@ $('.hamburger').on('click', function () {
 
 
 
+// --- Поочередная анимация появления characteristic
 $(document).ready(function () {
     var characteristicAnim = function () {
         var active = $('.characteristic__item.active');
@@ -56,7 +57,7 @@ $(document).ready(function () {
     }
 
     interval = setInterval(characteristicAnim, 5000);
-    
+
     $.fn.nextOrFirst = function (selector) {
         var next = this.next(selector);
         return (next.length) ? next : this.prevAll(selector).last();
@@ -73,17 +74,18 @@ $(document).ready(function () {
     })
 });
 
-// $(document).ready(function () {
-//     var interval = setInterval(function () {
-//         if ($('.characteristic__item:not(.active)').size() == 0) {
-//             clearInterval(interval);
-//         } else {
-//             $('.characteristic__item:not(.active)').first().addClass('animate');
-//         }
-//     }, 400);
-// });
 
 
+// --- Аккордион
+$(function () {
+    $('.accordion').find('.accordion__item-header').click(function () {
+
+        $('.accordion__item').removeClass('active');
+        $(this).next().slideDown('fast');
+        $('.accordion__item-answer').not($(this).next()).slideUp('slow');
+        $(this).parent().addClass('active');
+    });
+});
 
 
 
@@ -150,6 +152,90 @@ $(document).ready(function () {
 
 
 
+// --- Проверка геолокации и автоматическое определение языка страницы
+$.ajax({
+    url: "https://get.geojs.io/v1/ip/geo.js",
+    dataType: "jsonp",
+    jsonpCallback: "geoip",
+    success: function (data) {
+        // Коды русскоязычных стран
+        let countries = ["KZ", "UA", "RU", "BY", "UZ", "TM", "GE", "AZ", "MD", "KG"];
+
+        // Изменение атрибутта lang у html
+        if (countries.includes(data.country_code)) {
+            $("html").attr("lang", "ru");
+        } else {
+            $("html").attr("lang", "en");
+        }
+
+        // Перевод страницы в зависимости от атрибута lang
+        var tran = new Translater({
+            lang: `${$("html").attr("lang")}`
+        });
+
+        // Смена активного элемента в списке языков
+        if ($("html").attr("lang") === 'ru') {
+            $('.languages__list-item').first().html('rus')
+            $('.languages__list-item').last().html('eng')
+        } else {
+            $('.languages__list-item').first().html('eng')
+            $('.languages__list-item').last().html('rus')
+        }
+
+        // Смена Title
+        var url = window.location.href;
+        var page1 = new RegExp("/");
+
+        if (page1.test(url) && $("html").attr("lang") === 'ru') {
+            $("title").html("Проект 3");
+        } else {
+            $("title").html("Project 3");
+        }
+
+        // Функция проверки при нажатии
+        function checkLang() {
+            if ($('.languages__list-item.active').html() == 'rus') {
+                $("html").attr("lang", "ru");
+                tran.setLang('default');
+            } else {
+                $("html").attr("lang", "en");
+                tran.setLang('en');
+            }
+
+            if (page1.test(url) && $("html").attr("lang") === 'ru') {
+                $("title").html("Проект 3");
+            } else {
+                $("title").html("Project 3");
+            }
+
+        }
+
+        // Менять элемент в languages
+        $('.languages__list-item').click(function () {
+            if ($(this).hasClass('active') == false) {
+                let ln1 = $('.languages__item-eng').html();
+                let ln2 = $('.languages__item-rus').html();
+
+                $('.languages__item-eng').html(ln2);
+                $('.languages__item-rus').html(ln1);
+            }
+
+            checkLang();
+        });
+        $('.languages__icon').click(function () {
+            let ln1 = $('.languages__item-eng').html();
+            let ln2 = $('.languages__item-rus').html();
+
+            $('.languages__item-eng').html(ln2);
+            $('.languages__item-rus').html(ln1);
+
+            checkLang();
+        });
+    }
+});
+
+
+
 // --- Locomotive Scroll (Smoth)
 const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
@@ -163,16 +249,121 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 
 
-// --- Аккордион
-$(function () {
-    $('.accordion').find('.accordion__item-header').click(function () {
+// --- Custom Cursor
+var cursor = {
+    delay: 8,
+    _x: 0,
+    _y: 0,
+    endX: (window.innerWidth / 2),
+    endY: (window.innerHeight / 2),
+    cursorVisible: true,
+    cursorEnlarged: false,
+    $outline: document.querySelector('.cursor'),
 
-        $('.accordion__item').removeClass('active');
-        $(this).next().slideDown('fast');
-        $('.accordion__item-answer').not($(this).next()).slideUp('slow');
-        $(this).parent().addClass('active');
-    });
-});
+    init: function () {
+        // Set up element sizes
+        this.outlineSize = this.$outline.offsetWidth;
+
+        this.setupEventListeners();
+        this.animateDotOutline();
+    },
+
+    setupEventListeners: function () {
+        var self = this;
+
+        // Anchor hovering
+        document.querySelectorAll('input, textarea, button, .top__btn, .accordion__item-header').forEach(function (el) {
+            el.addEventListener('mouseover', function () {
+                self.cursorEnlarged = true;
+                $('.cursor').css('background-color', 'transparent');
+            });
+            el.addEventListener('mouseout', function () {
+                self.cursorEnlarged = false;
+                $('.cursor').css('background-color', 'rgba(255,255,255,1)');
+            });
+        });
+
+        // Anchor hovering
+        document.querySelectorAll('a, .languages__icon, .languages__list-item').forEach(function (el) {
+            el.addEventListener('mouseover', function () {
+                self.cursorEnlarged = true;
+                $('.cursor').css({ 'transform': 'translate(-50%, -50%) scale(0.5)' });
+            });
+            el.addEventListener('mouseout', function () {
+                self.cursorEnlarged = false;
+                $('.cursor').css({ 'transform': 'translate(-50%, -50%) scale(1)' });
+            });
+        });
+
+        // Click events
+        document.addEventListener('mousedown', function () {
+            self.cursorEnlarged = true;
+            self.toggleCursorSize();
+        });
+        document.addEventListener('mouseup', function () {
+            self.cursorEnlarged = false;
+            self.toggleCursorSize();
+        });
+
+
+        document.addEventListener('mousemove', function (e) {
+            // Show the cursor
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+
+            // Position the dot
+            self.endX = e.pageX;
+            self.endY = e.pageY;
+        });
+
+        // Hide/show cursor
+        document.addEventListener('mouseenter', function (e) {
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+            self.$outline.style.opacity = 1;
+        });
+
+        document.addEventListener('mouseleave', function (e) {
+            self.cursorVisible = true;
+            self.toggleCursorVisibility();
+            self.$outline.style.opacity = 0;
+        });
+    },
+
+    animateDotOutline: function () {
+        var self = this;
+
+        self._x += (self.endX - self._x) / self.delay;
+        self._y += (self.endY - self._y) / self.delay;
+        self.$outline.style.top = self._y + 'px';
+        self.$outline.style.left = self._x + 'px';
+
+        requestAnimationFrame(this.animateDotOutline.bind(self));
+    },
+
+    toggleCursorSize: function () {
+        var self = this;
+
+        if (self.cursorEnlarged) {
+            self.$outline.style.transform = 'translate(-50%, -50%) scale(1.5)';
+        } else {
+            self.$outline.style.transform = 'translate(-50%, -50%) scale(1)';
+        }
+    },
+
+    toggleCursorVisibility: function () {
+        var self = this;
+
+        if (self.cursorVisible) {
+            self.$outline.style.opacity = 1;
+        } else {
+            self.$outline.style.opacity = 0;
+        }
+    },
+
+}
+cursor.init();
+
 
 
 // --- Particles
@@ -262,265 +453,4 @@ function Circle() {
 
     this.getX = function () { return this.x; }
     this.getY = function () { return this.y; }
-}
-
-
-
-// --- Менять элемент в languages
-$('.languages__list-item').click(function () {
-    if ($(this).hasClass('active') == false) {
-        let ln1 = $('.languages__item-eng').html();
-        let ln2 = $('.languages__item-rus').html();
-
-        $('.languages__item-eng').html(ln2);
-        $('.languages__item-rus').html(ln1);
-    }
-});
-$('.languages__icon').click(function () {
-    let ln1 = $('.languages__item-eng').html();
-    let ln2 = $('.languages__item-rus').html();
-
-    $('.languages__item-eng').html(ln2);
-    $('.languages__item-rus').html(ln1);
-});
-
-
-// --- Проверка геолокации и автоматическое определение языка страницы
-// $.ajax({
-//     url: "https://get.geojs.io/v1/ip/geo.js",
-//     dataType: "jsonp",
-//     jsonpCallback: "geoip",
-//     success: function (data) {
-//         // Коды русскоязычных стран
-//         let countries = ["KZ", "UA", "RU", "BY", "UZ", "TM", "GE", "AZ", "MD", "KG"];
-
-//         // Изменение атрибутта lang у html
-//         if (countries.includes(data.country_code)) {
-//             $("html").attr("lang", "ru");
-//         } else {
-//             $("html").attr("lang", "en");
-//         }
-
-//         // Перевод страницы в зависимости от атрибута lang
-//         var tran = new Translater({
-//             lang: `${$("html").attr("lang")}`
-//         });
-
-//         // Смена активного элемента в списке языков
-//         if ($("html").attr("lang") === 'ru') {
-//             $('.simple-select ul li').removeClass('active');
-//             $('.simple-select ul li').first().addClass('active');
-//             $('.simple-select span').html('ru');
-//         } else {
-//             $('.simple-select ul li').removeClass('active');
-//             $('.simple-select ul li').last().addClass('active');
-//             $('.simple-select span').html('en');
-//         }
-
-//         // Смена языка в атрибутте lang
-//         $(".simple-select a").on("click", function (e) {
-//             e.preventDefault();
-//             $("html").attr("lang", $(this).text());
-//         });
-//         // Смена языка в списке
-//         $('.simple-select ul li a').first().click(function () {
-//             $(this).toggleClass('gg');
-//             tran.setLang('default');
-//         });
-//         $('.simple-select ul li a').last().click(function () {
-//             $(this).toggleClass('gg');
-//             tran.setLang('en');
-//         });
-//     }
-// });
-
-
-
-// --- Перевод страницы в зависимости от атрибута lang
-var tran = new Translater({
-    lang: `${$("html").attr("lang")}`
-});
-
-
-
-// --- Button в Footer
-document.querySelectorAll('.btn').forEach(button => button.innerHTML = '<div><span>' + button.textContent.trim().split('').join('</span><span>') + '</span></div>');
-
-
-
-// --- Custom Cursor
-var cursor = {
-    delay: 8,
-    _x: 0,
-    _y: 0,
-    endX: (window.innerWidth / 2),
-    endY: (window.innerHeight / 2),
-    cursorVisible: true,
-    cursorEnlarged: false,
-    $outline: document.querySelector('.cursor-dot-outline'),
-
-    init: function () {
-        // Set up element sizes
-        this.outlineSize = this.$outline.offsetWidth;
-
-        this.setupEventListeners();
-        this.animateDotOutline();
-    },
-
-    setupEventListeners: function () {
-        var self = this;
-
-        // Anchor hovering
-        document.querySelectorAll('a, .simple-select, .accordion__item-header, .search, .prev, .next, .swiper-pagination-bullet, input').forEach(function (el) {
-            el.addEventListener('mouseover', function () {
-                self.cursorEnlarged = true;
-                // self.toggleCursorSize();
-                $('.cursor-dot-outline').css('background-color', 'transparent');
-            });
-            el.addEventListener('mouseout', function () {
-                self.cursorEnlarged = false;
-                // self.toggleCursorSize();
-                $('.cursor-dot-outline').css('background-color', 'rgba(255,255,255,1)');
-            });
-        });
-
-        // Click events
-        document.addEventListener('mousedown', function () {
-            self.cursorEnlarged = true;
-            self.toggleCursorSize();
-        });
-        document.addEventListener('mouseup', function () {
-            self.cursorEnlarged = false;
-            self.toggleCursorSize();
-        });
-
-
-        document.addEventListener('mousemove', function (e) {
-            // Show the cursor
-            self.cursorVisible = true;
-            self.toggleCursorVisibility();
-
-            // Position the dot
-            self.endX = e.pageX;
-            self.endY = e.pageY;
-        });
-
-        // Hide/show cursor
-        document.addEventListener('mouseenter', function (e) {
-            self.cursorVisible = true;
-            self.toggleCursorVisibility();
-            self.$outline.style.opacity = 1;
-        });
-
-        document.addEventListener('mouseleave', function (e) {
-            self.cursorVisible = true;
-            self.toggleCursorVisibility();
-            self.$outline.style.opacity = 0;
-        });
-    },
-
-    animateDotOutline: function () {
-        var self = this;
-
-        self._x += (self.endX - self._x) / self.delay;
-        self._y += (self.endY - self._y) / self.delay;
-        self.$outline.style.top = self._y + 'px';
-        self.$outline.style.left = self._x + 'px';
-
-        requestAnimationFrame(this.animateDotOutline.bind(self));
-    },
-
-    toggleCursorSize: function () {
-        var self = this;
-
-        if (self.cursorEnlarged) {
-            self.$outline.style.transform = 'translate(-50%, -50%) scale(1.5)';
-        } else {
-            self.$outline.style.transform = 'translate(-50%, -50%) scale(1)';
-        }
-    },
-
-    toggleCursorVisibility: function () {
-        var self = this;
-
-        if (self.cursorVisible) {
-            self.$outline.style.opacity = 1;
-        } else {
-            self.$outline.style.opacity = 0;
-        }
-    },
-
-}
-cursor.init();
-
-
-
-
-
-
-// $(document).ready(function () {
-//     var interval = setInterval(function () {
-//         if ($('.characteristic__item:not(.active)').size() == 0) {
-//             clearInterval(interval);
-//         } else {
-//             $('.characteristic__item:not(.active)').first().addClass('animate');
-//         }
-//     }, 400);
-// });
-
-// $(function () {
-//     $.each($('.characteristic__item'), function (i, el) {
-//         setTimeout(function () {
-//             $('.characteristic__item').removeClass('active');
-//             $(el).addClass("active");
-//         }, 500 + (i * 500));
-//     });
-// });
-
-// var setTheInterval = null;
-// setTheInterval = setInterval(function () {
-//     $(".characteristic__item").removeClass('active');
-//     $(".characteristic__item:not(.active)").eq(0).addClass('active');
-// }, 800);
-
-// var imageArray = [];
-// var activeIndex = 0;
-// setInterval(function () {
-//     imageArray[activeIndex].removeClass('active');
-//     activeIndex++;
-//     activeIndex %= 4;
-//     imageArray[activeIndex].addClass('active');
-// }, 800);
-
-// $(document).ready(function () {
-//     $(window).scroll(function () {
-//         $('.characteristic').each(function () {
-//             setInterval(function () {
-//                 var active = $('.characteristic__item.active');
-//                 active.nextOrFirst().addClass('active');
-//                 active.removeClass('active');
-//             }, 5000);
-//             $.fn.nextOrFirst = function (selector) {
-//                 var next = this.next(selector);
-//                 return (next.length) ? next : this.prevAll(selector).last();
-//             };
-//         });
-//     });
-// });
-
-
-// $(document).ready(function () {
-
-// function slideIn() {
-//     var active = $('.characteristic__item.active');
-//     active.nextOrFirst().addClass('active');
-//     active.removeClass('active');
-// }
-// $.fn.nextOrFirst = function (selector) {
-//     var next = this.next(selector);
-//     return (next.length) ? next : this.prevAll(selector).last();
-// };
-
-
-
-;
+};
